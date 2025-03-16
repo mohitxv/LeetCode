725837a0eb5657f5d0770e6b4af11b435b81d862
mohitxv <117845136+mohitxv@users.noreply.github.com>
2025-03-16 17:47:11 +0530

    Time: 2 ms (13.39%) | Memory: 8.7 MB (83.88%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto it:s){
+            if(it == '(' || it == '{' || it == '[') st.push(it);
+            else{
+                if(st.empty()) return false;
+                if(it == ')' && st.top()!= '(' ||
+                   it == '}' && st.top()!= '{' ||
+                   it == ']' && st.top()!= '['){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
